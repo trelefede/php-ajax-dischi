1dcd3b9f5aa89d69e331473e918ee0233aac6b87
TreleaniFederico <treleani.federico@gmail.com>
2022-10-19 19:43:33 +0200

    add html containers and styles make up

--- index.php
+++ index.php
@@ -4,20 +4,36 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/style/style.css">
+    <link rel="stylesheet" href="style/style.css">
     <title>PHP DISCS</title>
 </head>
 <body>
+
+<div class="container">
+    <div class="album-container">
 
 <?php
 
 include __DIR__ . '/data.php';
 
 foreach ($albums as $album){
-    echo $album['title'];
+    ?>
+
+        <div class="card-container">
+
+            <?php
+                include __DIR__ . '/card-album.php'
+            ?>
+
+        </div>
+
+    <?php
 }
 
 ?>
+
+    </div>
+</div>  
 
 
     
